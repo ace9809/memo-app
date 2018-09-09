6272825df352da466351d94f428f6a448944e33a
ace9809 <tktmdwns@naver.com>
2018-09-10 03:19:00 +0900

    Feature:createMemo api 생성

--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -6,5 +6,8 @@ import axios from 'axios';
 export default {
   getInitialMemo() {
    return axios.get('http://114.207.113.7:11111/memo-list/');
+  },
+  createMemo() {
+    return axios.post('http://114.207.113.7:11111/memo-list/create/');
   }
 }
