7867fc56a7477602c2798756038bf8a8ac953d9a
ace9809 <tktmdwns@naver.com>
2018-09-09 18:07:38 +0900

    Feature: Header container ui 개발

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,11 +2,20 @@
  * Created by Ace on 2018. 9. 8..
  */
 import React, { Component } from 'react';
+import styled from 'styled-components';
+import Header from './Header'
+
+const Wrapper = styled.div`
+  width: 100%;
+  min-height: 100%;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div>test</div>
+      <Wrapper>
+        <Header />
+      </Wrapper>
     )
   }
 }
---
+++ src/containers/Header.js
@@ -0,0 +1,51 @@
+/**
+ * Created by Ace on 2018. 9. 9..
+ */
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const HeaderWrapper = styled.div`
+  width: 100%;
+  height: 77px;
+  background-color: #fff;
+  border-bottom: 1px solid #F3F1F1;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+const NavWrapper = styled.div`
+  margin-right: 35px;
+  display: flex;
+`;
+
+const Button = styled.button`
+  border-radius: 6px;
+  width: 80px;
+  height: 40px;
+  cursor: pointer;
+  margin-left: 20px;
+  background-color: ${(props) => props.color};
+  outline: 0;
+  border: 0;
+  color: white;
+`;
+
+class Header extends Component {
+  render() {
+    return (
+      <HeaderWrapper>
+        <NavWrapper>
+          <Button color={'#5cb85c'}>
+            작성하기
+          </Button>
+          <Button color={'#d9534f'}>
+            삭제하기
+          </Button>
+        </NavWrapper>
+      </HeaderWrapper>
+    )
+  }
+}
+
+export default Header;
