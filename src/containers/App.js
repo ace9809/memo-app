8d445c5278948a60f51e42b2c98000586e150c7b
ace9809 <tktmdwns@naver.com>
2018-09-09 14:51:33 +0900

    Feature: 첫 화면에 App component 호출

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Ace on 2018. 9. 8..
+ */
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>test</div>
+    )
+  }
+}
+
+export default App;
