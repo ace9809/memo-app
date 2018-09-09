021f45cbb232762cd83fcf514c0a94cfbd1fe8fd
ace9809 <tktmdwns@naver.com>
2018-09-09 19:14:16 +0900

    Feature: action constants파일에 정리

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Ace on 2018. 9. 8..
+ */
+export const ADD_MEMO = 'ADD_MEMO';
+export const REMOVE_MEMO = 'REMOVE_MEMO';
+export const MODIFY_MEMO = 'MODIFY_MEMO';
+export const GET_MEMOLIST = 'GET_MEMOLIST';
