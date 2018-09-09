68b87b27cf5e011e4a422a96fe323587ea72cc5e
ace9809 <tktmdwns@naver.com>
2018-09-10 00:47:02 +0900

    Feature:getMemoList 리듀서 생성

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,25 @@
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  memo
 });
+
+function memo(state = {}, action) {
+  switch(action.type) {
+    case 'GET_MEMOLIST':
+      console.log('action', [
+      ...state,
+      action.payload.data
+    ])
+      return [
+        ...state,
+        action.payload.data
+      ]
+    default:
+      return state
+
+  }
+
+}
 
 export default rootReducer;
