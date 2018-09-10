7898eca30f4fab9ffd81b854afff84a81a97264e
ace9809 <tktmdwns@naver.com>
2018-09-10 23:04:09 +0900

    constants 파일에 GET_MEMO 상추 추가

--- src/actions/constants.js
+++ src/actions/constants.js
@@ -5,3 +5,4 @@ export const ADD_MEMO = 'ADD_MEMO';
 export const REMOVE_MEMO = 'REMOVE_MEMO';
 export const MODIFY_MEMO = 'MODIFY_MEMO';
 export const GET_MEMOLIST = 'GET_MEMOLIST';
+export const GET_MEMO = 'GET_MEMO';
