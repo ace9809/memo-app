136ad4e08e9bd6a705c5a0a873af2738258ee16a
ace9809 <tktmdwns@naver.com>
2018-09-10 23:05:36 +0900

    Feature:MemoForm container redux와 연결

--- src/containers/MemoForm.js
+++ src/containers/MemoForm.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import styled from 'styled-components';
+import { getMemo } from '../actions';
 
-const MemoForm = () => {
-  return (
-  <div>test</div>
-  )
+class MemoForm extends Component {
+  componentWillMount() {
+    this.props.getMemo(this.props.id);
+  }
+  render() {
+    if (!this.props.memo) {
+      return <div>로딩 중...</div>
+    }
+    const {
+      title,
+      description
+    } = this.props.memo;
+    return (
+     <div>
+       {description}
+     </div>
+    )
+  }
 }
 
-export default MemoForm;
+function mapStateToProps(state) {
+  console.log('state', state);
+  return {
+    memo: state.memo.memo
+  }
+}
+
+export default connect(mapStateToProps, { getMemo })(MemoForm);
