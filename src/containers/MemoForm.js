509211a1612d5aaf676104d39fe378be21c61929
ace9809 <tktmdwns@naver.com>
2018-09-11 02:55:51 +0900

    Feature:메포폼 컨테이너 구현

--- src/containers/MemoForm.js
+++ src/containers/MemoForm.js
@@ -1,30 +1,96 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
+import SimpleMDE from 'react-simplemde-editor';
 import { getMemo } from '../actions';
 
+const Wrapper = styled.div`
+  width: 100%;
+`;
+
+const TitleWrapper = styled.div`
+  display: flex;
+`;
+
+const EditorWrapper = styled.div`
+  padding: 40px 25px;
+`;
+
+const TitleInput = styled.input`
+  margin: 40px 25px;
+  width: 100%;
+  display: block;
+  border: none;
+  padding: 10px 0;
+  border-bottom: solid 1px #1abc9c;
+  background-repeat: no-repeat;
+  color: #0e6252;
+  &:focus, &:valid {
+  box-shadow: none;
+  outline: none;
+  background-position: 0 0;
+  }
+`;
+
 class MemoForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      description: this.props.description
+    };
+    // this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleTitleChange = event => {
+    this.setState({ title: event.target.value });
+  }
+
+  handleDescriptionChange = value => {
+    this.setState({ description: value });
+  }
+
   componentWillMount() {
     this.props.getMemo(this.props.id);
+    if (this.props.memo) {
+      this.setState({
+        title: this.props.memo.title
+      })
+    }
   }
   render() {
     if (!this.props.memo) {
       return <div>로딩 중...</div>
     }
-    const {
-      title,
-      description
-    } = this.props.memo;
+
+    const editorOptions = {
+      autofocus: false,
+      spellChecker: false,
+      placeholder: "입력해주세요...",
+      initialValue: this.props.memo.description,
+    }
     return (
-     <div>
-       {description}
-     </div>
+     <Wrapper>
+       <TitleWrapper>
+         <TitleInput
+           type="text"
+           value={this.state.title}
+           onChange={this.handleTitleChange}
+         />
+       </TitleWrapper>
+       <EditorWrapper>
+         <SimpleMDE
+           options={editorOptions}
+           value={this.state.description}
+           onChange={this.handleDescriptionChange}
+         />
+       </EditorWrapper>
+     </Wrapper>
     )
   }
 }
 
 function mapStateToProps(state) {
-  console.log('state', state);
   return {
     memo: state.memo.memo
   }
