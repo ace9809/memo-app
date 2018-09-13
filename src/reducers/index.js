3b81cbd8b6a3d4a6e6799b19725d895f82f78c3e
ace9809 <tktmdwns@naver.com>
2018-09-13 20:57:17 +0900

    Fix:리듀서 리턴하는 로직 수정

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,11 @@
 import { combineReducers } from 'redux';
+import _ from 'lodash';
 
 const rootReducer = combineReducers({
   memo
 });
 
-const INITIAL_STATE = { all: [], memo: null}
-
-function memo(state = [], action) {
-  console.log('action', action);
+function memo(state = { all: [], memo: null} , action) {
   switch(action.type) {
     case 'GET_MEMOLIST':
       return {
@@ -22,12 +20,17 @@ function memo(state = [], action) {
     case 'ADD_MEMO':
       return {
       ...state,
-      all: action.payload.data
+      all: state.all.concat(action.payload.data)
     }
     case 'MODIFY_MEMO':
       return {
         ...state,
         memo: action.payload.data
+      }
+    case 'DELETE_MEMO':
+      return {
+        ...state,
+        all: _.filter(state.all, (memo) => memo.id !== Number(action.payload))
       }
     default:
       return state
