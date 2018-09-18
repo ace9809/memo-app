259009dd1af93709850a70ee96e492c0a55f6027
ace9809 <tktmdwns@naver.com>
2018-09-18 19:14:05 +0900

    Feature:Modal 컴포넌트 연결

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -4,6 +4,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
+import Modal from '../components/Modal/index';
 import { addMemo } from '../actions'
 
 const HeaderWrapper = styled.div`
@@ -40,13 +41,31 @@ const Button = styled.button`
 `;
 
 class Header extends Component {
+  state = {
+    open: false,
+  };
+
+  onOpenModal = () => {
+    this.setState({ open: true });
+  };
+
+  onCloseModal = () => {
+    this.setState({ open: false });
+  };
+
   render() {
+    const { open } = this.state;
     return (
       <HeaderWrapper>
         <TitleWrapper>
           Symphony Note
         </TitleWrapper>
+        <Modal open={open} onClose={this.onCloseModal} center>
+        </Modal>
         <NavWrapper>
+          <Button color={'#5cb85c'} onClick={this.onOpenModal}>
+            공지사항
+          </Button>
           <Button color={'#5cb85c'} onClick={this.props.addMemo}>
             작성하기
           </Button>
